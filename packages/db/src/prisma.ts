1f1de85d82f20b22bb95c478aebf7b86a40173db
Jan Piotrowski <piotrowski+github@gmail.com>
2024-04-10 17:10:26 +0000

    fix import

--- packages/db/src/prisma.ts
+++ packages/db/src/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from './pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from './__generated__/edge';
+import { PrismaClient } from './__generated__';
 
 export const prisma = (
   connectionString: string,
